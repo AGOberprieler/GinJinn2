88fcab5bfbecf22809c772b1c8ae80a5817a7371
TankredO <tankred.ott@ur.de>
2021-08-07 11:01:49 +0200

    input processing init

--- ginjinn/commandline/commandline_app/utils/sw_split.py
+++ ginjinn/commandline/commandline_app/utils/sw_split.py
@@ -1,22 +1,28 @@
 ''' ginjinn utils sw_split functionality
 '''
 
+import warnings
 import os
 import sys
 import shutil
+from typing import Iterable, Tuple
 
-def utils_sw_split(args):
-    '''utils_sw_split
 
-    GinJinn utils utils_sw_split command.
+def process_window_size(
+    window_size: Iterable[int],
+) -> Tuple[int, int]:
+    '''process_window_size
 
     Parameters
     ----------
-    args
-        Parsed GinJinn commandline arguments for the ginjinn utils
-        sw_split subcommand.
+    window_size : Iterable[int]
+        window_size command line argument(s)
+
+    Returns
+    -------
+    Tuple[int, int]
+        width, height
     '''
-    window_size = args.window_size
     if len(window_size) == 1:
         win_width = window_size[0]
         win_height = window_size[0]
@@ -25,9 +31,25 @@ def utils_sw_split(args):
         win_height = window_size[1]
     else:
         print('ERROR: "-s/--window_size" must receive 1 or 2 arguments.')
-        return
+        sys.exit(1)
 
-    overlap = args.overlap
+    return win_width, win_height
+
+def process_overlap(
+    overlap: Iterable[int],
+) -> Tuple[int, int]:
+    '''process_overlap
+
+    Parameters
+    ----------
+    overlap : Iterable[int]
+        overlap command line argument(s)
+
+    Returns
+    -------
+    Tuple[int, int]
+        horizontal, vertical
+    '''
     if len(overlap) == 1:
         hor_overlap = overlap[0]
         vert_overlap = overlap[0]
@@ -36,131 +58,65 @@ def utils_sw_split(args):
         vert_overlap = overlap[1]
     else:
         print('ERROR: "-p/--overlap" must receive 1 or 2 arguments.')
-        return
+        sys.exit(1)
 
-    from ginjinn.utils.dataset_cropping import sliding_window_crop_coco, sliding_window_crop_pvoc
+    return hor_overlap, vert_overlap
+
+def utils_sw_split(args):
+    '''utils_sw_split
+
+    GinJinn utils sw_split command.
+
+    Parameters
+    ----------
+    args
+        Parsed GinJinn commandline arguments for the ginjinn utils
+        sw_split subcommand.
+    '''
+
+    win_width, win_height = process_window_size(args.window_size)
+    hor_overlap, vert_overlap = process_overlap(args.overlap)
+
+    print(win_width, win_height)
+    print(hor_overlap, vert_overlap)
+
+    image_dir = args.image_dir
+    ann_path = args.ann_path
+    dataset_dir = args.dataset_dir
+
+    if dataset_dir is not None:
+        warn_msgs = []
+        if ann_path is not None:
+            warn_msgs.append(
+                'WARNING: -I/--dataset_dir was provided. -a/--ann_path will be ignored.'
+            )
+        if image_dir is not None:
+            warn_msgs.append(
+                'WARNING: -I/--dataset_dir was provided. -i/--image_dir will be ignored.'
+            )
+        if len(warn_msgs) > 0:
+            print('\n'.join(warn_msgs))
+        print('Will use -I/--dataset_dir')
+
+    elif ann_path is not None and image_dir is not None:
+        print('Will use -a/--ann_path and -i/--image_dir')
+    elif image_dir is not None:
+        print('Will use -i/--image_dir')
+    else:
+        print(
+            'ERROR: invalid input arguments. Provide either -I/--datset_dir, or -i/--image_dir, '
+            'or -i/--image_dir AND -a/--ann_path.'
+        )
+        sys.exit(1)
+
+    print('image_dir:', image_dir)
+    print('ann_path:', ann_path)
+    print('dataset_dir:', dataset_dir)
+
     from ginjinn.utils import confirmation_cancel
 
-    if os.path.exists(args.out_dir):
-        msg = f'Directory "{args.out_dir} already exists. Should it be overwritten?"\n' +\
-            f'WARNING: This will remove "{args.out_dir}" and ALL SUBDIRECTORIES.\n'
-        should_remove = confirmation_cancel(msg)
-        if should_remove:
-            shutil.rmtree(args.out_dir)
-            os.mkdir(args.out_dir)
-        else:
-            print('sw_split canceled')
-            return
-    else:
-        os.mkdir(args.out_dir)
+    # image splitting
+    from ginjinn.utils.dataset_cropping import sliding_window_crop_images
 
-
-    from ginjinn.utils.utils import get_dstype
-
-    ann_type = args.ann_type
-    # infer ann_type
-    if ann_type == 'auto':
-        ds_types = []
-        for ds_name in ['train', 'val', 'test']:
-            ds_path = os.path.join(args.split_dir, ds_name)
-            if not os.path.isdir(ds_path):
-                continue
-            ds_types.append(get_dstype(ds_path))
-        if len(set(ds_types)) > 1:
-            print(
-                f'ERROR: Found multiple dataset types in "{args.split_dir}". ' +\
-                'The datasets in splitdir must all have the same annotation type (COCO or PVOC).'
-            )
-            sys.exit()
-        if len(ds_types) < 1:
-            print(
-                f'ERROR: Could not find any dataset (train, val, test) in "{args.split_dir}".'
-            )
-            sys.exit()
-        ann_type = ds_types[0]
-
-    if ann_type == 'COCO':
-        for ds_name in ['train', 'val', 'test']:
-            img_dir = os.path.join(args.split_dir, ds_name, 'images')
-            ann_path = os.path.join(args.split_dir, ds_name, 'annotations.json')
-
-            if not os.path.isdir(img_dir):
-                print(
-                    f'No image directory found for dataset "{ds_name}". ' +\
-                    f'(Expected location: "{img_dir}")'
-                )
-                continue
-            if not os.path.isfile(ann_path):
-                print(
-                    f'No annotation file found for dataset "{ds_name}". ' +\
-                    f'(Expected location: "{ann_path}")'
-                )
-                continue
-
-            img_dir_out = os.path.join(args.out_dir, ds_name, 'images')
-            os.makedirs(img_dir_out, exist_ok=True)
-            ann_path_out = os.path.join(args.out_dir, ds_name, 'annotations.json')
-
-            print(f'Splitting dataset {ds_name}...')
-            sliding_window_crop_coco(
-                img_dir=img_dir,
-                ann_path=ann_path,
-                img_dir_out=img_dir_out,
-                ann_path_out=ann_path_out,
-                win_width=win_width,
-                win_height=win_height,
-                hor_overlap=hor_overlap,
-                vert_overlap=vert_overlap,
-                img_id=args.img_id,
-                obj_id=args.obj_id,
-                save_empty=not args.remove_empty,
-                keep_incomplete=not args.remove_incomplete,
-                task=args.task,
-            )
-
-            msg = \
-                f'Sliding-window images for dataset {ds_name} written to {img_dir_out}. '+\
-                f'Sliding-window annotation for dataset {ds_name} written to {ann_path_out}.'
-            print(msg)
-
-    elif ann_type == 'PVOC':
-        for ds_name in ['train', 'val', 'test']:
-            img_dir = os.path.join(args.split_dir, ds_name, 'images')
-            ann_dir = os.path.join(args.split_dir, ds_name, 'annotations')
-
-            if not os.path.isdir(img_dir):
-                print(
-                    f'No image directory found for dataset "{ds_name}". ' +\
-                    f'(Expected location: "{img_dir}")'
-                )
-                continue
-            if not os.path.isdir(ann_dir):
-                print(
-                    f'No annotation directory found for dataset "{ds_name}". ' +\
-                    f'(Expected location: "{ann_dir}")'
-                )
-                continue
-
-            img_dir_out = os.path.join(args.out_dir, ds_name, 'images')
-            os.makedirs(img_dir_out, exist_ok=True)
-            ann_dir_out = os.path.join(args.out_dir, ds_name, 'annotations')
-            os.makedirs(ann_dir_out, exist_ok=True)
-
-            print(f'Splitting dataset {ds_name}...')
-            sliding_window_crop_pvoc(
-                img_dir=img_dir,
-                ann_dir=ann_dir,
-                img_dir_out=img_dir_out,
-                ann_dir_out=ann_dir_out,
-                win_width=win_width,
-                win_height=win_height,
-                hor_overlap=hor_overlap,
-                vert_overlap=vert_overlap,
-                save_empty=not args.remove_empty,
-                keep_incomplete=not args.remove_incomplete,
-            )
-
-            msg = \
-                f'Sliding-window images for dataset {ds_name} written to {img_dir_out}. '+\
-                f'Sliding-window annotations for dataset {ds_name} written to {ann_dir_out}.'
-            print(msg)
+    # dataset splitting
+    from ginjinn.utils.dataset_cropping import sliding_window_crop_coco, sliding_window_crop_pvoc
