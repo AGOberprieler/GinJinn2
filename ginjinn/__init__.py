6cf0506e09829e45f27e4cd3ffe11f8c43699f79
TankredO <ott.tankred@gmail.com>
2022-02-05 17:21:34 +0100

    small fixes, drop python 3.9

--- ginjinn/__init__.py
+++ ginjinn/__init__.py
@@ -1,25 +1,4 @@
-''' GinJinn
-
-Summary
--------
-GinJinn is an object detection framework for the detection of structures in
-digitized herbarium specimens.
-GinJinn includes bounding-box detection and segmentation functionality using
-state of the art computer vision models.
-Further, it includes functionality for loading und exporting data in most
-of the common data formats.
-
-References
-----------
-Ott, T., C. Palm, R. Vogt, and C. Oberprieler. 2020. GinJinn: An object-detection
-pipeline for automated feature extraction from herbarium specimens.
-Applications in Plant Sciences 2020 8(6).
+''' GinJinn2
 '''
-__version__ = '1.0.0'
 
-import pkg_resources
-
-# an example for loading package data
-example_data_path = pkg_resources.resource_filename('ginjinn', 'data/example_data.txt')
-with open(example_data_path) as f:
-    example_data = f.read()
+__version__ = '1.0.3'
