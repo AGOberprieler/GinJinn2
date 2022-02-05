a7043a489de5d2cf1605c196e8354cb36036723f
TankredO <ott.tankred@gmail.com>
2022-02-05 14:45:19 +0100

    pspnet torch>1.5 fix

--- ginjinn/segmentation_refinement/eval_helper.py
+++ ginjinn/segmentation_refinement/eval_helper.py
@@ -1,14 +1,24 @@
 import torch
 import torch.nn.functional as F
 
+
 def resize_max_side(im, size, method):
     h, w = im.shape[-2:]
     max_side = max(h, w)
     ratio = size / max_side
     if method in ['bilinear', 'bicubic']:
-        return F.interpolate(im, scale_factor=ratio, mode=method, align_corners=False, recompute_scale_factor=True)
+        return F.interpolate(
+            im,
+            scale_factor=ratio,
+            mode=method,
+            align_corners=False,
+            recompute_scale_factor=True,
+        )
     else:
-        return F.interpolate(im, scale_factor=ratio, mode=method, recompute_scale_factor=True)
+        return F.interpolate(
+            im, scale_factor=ratio, mode=method, recompute_scale_factor=True
+        )
+
 
 def safe_forward(model, im, seg, inter_s8=None, inter_s4=None):
     """
@@ -16,37 +26,38 @@ def safe_forward(model, im, seg, inter_s8=None, inter_s4=None):
     """
     b, _, ph, pw = seg.shape
     if (ph % 8 != 0) or (pw % 8 != 0):
-        newH = ((ph//8+1)*8)
-        newW = ((pw//8+1)*8)
+        newH = (ph // 8 + 1) * 8
+        newW = (pw // 8 + 1) * 8
         p_im = torch.zeros(b, 3, newH, newW, device=im.device)
         p_seg = torch.zeros(b, 1, newH, newW, device=im.device) - 1
 
-        p_im[:,:,0:ph,0:pw] = im
-        p_seg[:,:,0:ph,0:pw] = seg
+        p_im[:, :, 0:ph, 0:pw] = im
+        p_seg[:, :, 0:ph, 0:pw] = seg
         im = p_im
         seg = p_seg
 
         if inter_s8 is not None:
             p_inter_s8 = torch.zeros(b, 1, newH, newW, device=im.device) - 1
-            p_inter_s8[:,:,0:ph,0:pw] = inter_s8
+            p_inter_s8[:, :, 0:ph, 0:pw] = inter_s8
             inter_s8 = p_inter_s8
         if inter_s4 is not None:
             p_inter_s4 = torch.zeros(b, 1, newH, newW, device=im.device) - 1
-            p_inter_s4[:,:,0:ph,0:pw] = inter_s4
+            p_inter_s4[:, :, 0:ph, 0:pw] = inter_s4
             inter_s4 = p_inter_s4
 
     images = model(im, seg, inter_s8, inter_s4)
     return_im = {}
 
     for key in ['pred_224', 'pred_28_3', 'pred_56_2']:
-        return_im[key] = images[key][:,:,0:ph,0:pw]
+        return_im[key] = images[key][:, :, 0:ph, 0:pw]
     del images
 
     return return_im
 
+
 def process_high_res_im(model, im, seg, L=900):
 
-    stride = L//2
+    stride = L // 2
 
     _, _, h, w = seg.shape
 
@@ -67,7 +78,7 @@ def process_high_res_im(model, im, seg, L=900):
 
     pred_224 = images['pred_224']
     pred_56 = images['pred_56_2']
-    
+
     """
     Local step
     """
@@ -80,16 +91,23 @@ def process_high_res_im(model, im, seg, L=900):
         combined_224 = torch.zeros_like(seg_small)
         combined_weight = torch.zeros_like(seg_small)
 
-        r_pred_224 = (F.interpolate(pred_224, size=(h, w), mode='bilinear', align_corners=False, recompute_scale_factor=True)>0.5).float()*2-1
-        r_pred_56 = F.interpolate(pred_56, size=(h, w), mode='bilinear', align_corners=False, recompute_scale_factor=True)*2-1
+        r_pred_224 = (
+            F.interpolate(pred_224, size=(h, w), mode='bilinear', align_corners=False)
+            > 0.5
+        ).float() * 2 - 1
+        r_pred_56 = (
+            F.interpolate(pred_56, size=(h, w), mode='bilinear', align_corners=False)
+            * 2
+            - 1
+        )
 
         padding = 16
-        step_size = stride - padding*2
-        step_len  = L
+        step_size = stride - padding * 2
+        step_len = L
 
         used_start_idx = {}
-        for x_idx in range((w)//step_size+1):
-            for y_idx in range((h)//step_size+1):
+        for x_idx in range((w) // step_size + 1):
+            for y_idx in range((h) // step_size + 1):
 
                 start_x = x_idx * step_size
                 start_y = y_idx * step_size
@@ -111,22 +129,24 @@ def process_high_res_im(model, im, seg, L=900):
                 end_y = min(h, end_y)
 
                 # The same crop might appear twice due to bounding/shifting
-                start_idx = start_y*w + start_x
+                start_idx = start_y * w + start_x
                 if start_idx in used_start_idx:
                     continue
                 else:
                     used_start_idx[start_idx] = True
-                
+
                 # Take crop
-                im_part = im_small[:,:,start_y:end_y, start_x:end_x]
-                seg_224_part = r_pred_224[:,:,start_y:end_y, start_x:end_x]
-                seg_56_part = r_pred_56[:,:,start_y:end_y, start_x:end_x]
+                im_part = im_small[:, :, start_y:end_y, start_x:end_x]
+                seg_224_part = r_pred_224[:, :, start_y:end_y, start_x:end_x]
+                seg_56_part = r_pred_56[:, :, start_y:end_y, start_x:end_x]
 
                 # Skip when it is not an interesting crop anyway
-                seg_part_norm = (seg_224_part>0).float()
+                seg_part_norm = (seg_224_part > 0).float()
                 high_thres = 0.9
                 low_thres = 0.1
-                if (seg_part_norm.mean() > high_thres) or (seg_part_norm.mean() < low_thres):
+                if (seg_part_norm.mean() > high_thres) or (
+                    seg_part_norm.mean() < low_thres
+                ):
                     continue
                 grid_images = safe_forward(model, im_part, seg_224_part, seg_56_part)
                 grid_pred_224 = grid_images['pred_224']
@@ -149,21 +169,28 @@ def process_high_res_im(model, im, seg, L=900):
                     end_y -= padding
                     pred_ey -= padding
 
-                combined_224[:,:,start_y:end_y, start_x:end_x] += grid_pred_224[:,:,pred_sy:pred_ey,pred_sx:pred_ex]
+                combined_224[:, :, start_y:end_y, start_x:end_x] += grid_pred_224[
+                    :, :, pred_sy:pred_ey, pred_sx:pred_ex
+                ]
 
                 del grid_pred_224
 
                 # Used for averaging
-                combined_weight[:,:,start_y:end_y, start_x:end_x] += 1
+                combined_weight[:, :, start_y:end_y, start_x:end_x] += 1
 
         # Final full resolution output
-        seg_norm = (r_pred_224/2+0.5)
+        seg_norm = r_pred_224 / 2 + 0.5
         pred_224 = combined_224 / combined_weight
-        pred_224 = torch.where(combined_weight==0, seg_norm, pred_224)
+        pred_224 = torch.where(combined_weight == 0, seg_norm, pred_224)
 
     _, _, h, w = seg.shape
     images = {}
-    images['pred_224'] = F.interpolate(pred_224, size=(h, w), mode='bilinear', align_corners=True, recompute_scale_factor=True)
+    images['pred_224'] = F.interpolate(
+        pred_224,
+        size=(h, w),
+        mode='bilinear',
+        align_corners=True,
+    )
 
     return images['pred_224']
 
@@ -185,8 +212,17 @@ def process_im_single_pass(model, im, seg, L=900):
     images = safe_forward(model, im, seg)
 
     if max(h, w) < L:
-        images['pred_224'] = F.interpolate(images['pred_224'], size=(h, w), mode='area', recompute_scale_factor=True)
+        images['pred_224'] = F.interpolate(
+            images['pred_224'],
+            size=(h, w),
+            mode='area',
+        )
     elif max(h, w) > L:
-        images['pred_224'] = F.interpolate(images['pred_224'], size=(h, w), mode='bilinear', align_corners=True, recompute_scale_factor=True)
+        images['pred_224'] = F.interpolate(
+            images['pred_224'],
+            size=(h, w),
+            mode='bilinear',
+            align_corners=True,
+        )
 
     return images['pred_224']
--- ginjinn/segmentation_refinement/models/psp/pspnet.py
+++ ginjinn/segmentation_refinement/models/psp/pspnet.py
@@ -9,8 +9,12 @@ class PSPModule(nn.Module):
     def __init__(self, features, out_features=1024, sizes=(1, 2, 3, 6)):
         super().__init__()
         self.stages = []
-        self.stages = nn.ModuleList([self._make_stage(features, size) for size in sizes])
-        self.bottleneck = nn.Conv2d(features * (len(sizes) + 1), out_features, kernel_size=1)
+        self.stages = nn.ModuleList(
+            [self._make_stage(features, size) for size in sizes]
+        )
+        self.bottleneck = nn.Conv2d(
+            features * (len(sizes) + 1), out_features, kernel_size=1
+        )
         self.relu = nn.ReLU(inplace=True)
 
     def _make_stage(self, features, size):
@@ -20,7 +24,15 @@ class PSPModule(nn.Module):
 
     def forward(self, feats):
         h, w = feats.size(2), feats.size(3)
-        set_priors = [F.interpolate(input=stage(feats), size=(h, w), mode='bilinear', align_corners=False, recompute_scale_factor=True) for stage in self.stages]
+        set_priors = [
+            F.interpolate(
+                input=stage(feats),
+                size=(h, w),
+                mode='bilinear',
+                align_corners=False,
+            )
+            for stage in self.stages
+        ]
         priors = set_priors + [feats]
         bottle = self.bottleneck(torch.cat(priors, 1))
         return self.relu(bottle)
@@ -50,7 +62,13 @@ class PSPUpsample(nn.Module):
         self.shortcut = nn.Conv2d(x_channels, out_channels, kernel_size=1)
 
     def forward(self, x, up):
-        x = F.interpolate(input=x, scale_factor=2, mode='bilinear', align_corners=False, recompute_scale_factor=True)
+        x = F.interpolate(
+            input=x,
+            scale_factor=2,
+            mode='bilinear',
+            align_corners=False,
+            recompute_scale_factor=True,
+        )
 
         p = self.conv(torch.cat([x, up], 1))
         sc = self.shortcut(x)
@@ -69,10 +87,10 @@ class RefinementModule(nn.Module):
         self.feats = extractors.resnet50()
         self.psp = PSPModule(2048, 1024, (1, 2, 3, 6))
 
-        self.up_1 = PSPUpsample(1024, 1024+256, 512)
-        self.up_2 = PSPUpsample(512, 512+64, 256)
-        self.up_3 = PSPUpsample(256, 256+3, 32)
-        
+        self.up_1 = PSPUpsample(1024, 1024 + 256, 512)
+        self.up_2 = PSPUpsample(512, 512 + 64, 256)
+        self.up_3 = PSPUpsample(256, 256 + 3, 32)
+
         self.final_28 = nn.Sequential(
             nn.Conv2d(1024, 32, kernel_size=1),
             nn.ReLU(inplace=True),
@@ -85,7 +103,7 @@ class RefinementModule(nn.Module):
             nn.Conv2d(32, 1, kernel_size=1),
         )
 
-        self.final_11 = nn.Conv2d(32+3, 32, kernel_size=1)
+        self.final_11 = nn.Conv2d(32 + 3, 32, kernel_size=1)
         self.final_21 = nn.Conv2d(32, 1, kernel_size=1)
 
     def forward(self, x, seg, inter_s8=None, inter_s4=None):
@@ -98,11 +116,17 @@ class RefinementModule(nn.Module):
         if inter_s8 is None:
             p = torch.cat((x, seg, seg, seg), 1)
 
-            f, f_1, f_2 = self.feats(p) 
+            f, f_1, f_2 = self.feats(p)
             p = self.psp(f)
 
             inter_s8 = self.final_28(p)
-            r_inter_s8 = F.interpolate(inter_s8, scale_factor=8, mode='bilinear', align_corners=False, recompute_scale_factor=True)
+            r_inter_s8 = F.interpolate(
+                inter_s8,
+                scale_factor=8,
+                mode='bilinear',
+                align_corners=False,
+                recompute_scale_factor=True,
+            )
             r_inter_tanh_s8 = torch.tanh(r_inter_s8)
 
             images['pred_28'] = torch.sigmoid(r_inter_s8)
@@ -116,16 +140,24 @@ class RefinementModule(nn.Module):
         if inter_s4 is None:
             p = torch.cat((x, seg, r_inter_tanh_s8, r_inter_tanh_s8), 1)
 
-            f, f_1, f_2 = self.feats(p) 
+            f, f_1, f_2 = self.feats(p)
             p = self.psp(f)
             inter_s8_2 = self.final_28(p)
-            r_inter_s8_2 = F.interpolate(inter_s8_2, scale_factor=8, mode='bilinear', align_corners=False, recompute_scale_factor=True)
+            r_inter_s8_2 = F.interpolate(
+                inter_s8_2,
+                scale_factor=8,
+                mode='bilinear',
+                align_corners=False,
+                recompute_scale_factor=True,
+            )
             r_inter_tanh_s8_2 = torch.tanh(r_inter_s8_2)
 
             p = self.up_1(p, f_2)
 
             inter_s4 = self.final_56(p)
-            r_inter_s4 = F.interpolate(inter_s4, scale_factor=4, mode='bilinear', align_corners=False)
+            r_inter_s4 = F.interpolate(
+                inter_s4, scale_factor=4, mode='bilinear', align_corners=False
+            )
             r_inter_tanh_s4 = torch.tanh(r_inter_s4)
 
             images['pred_28_2'] = torch.sigmoid(r_inter_s8_2)
@@ -141,17 +173,28 @@ class RefinementModule(nn.Module):
         """
         p = torch.cat((x, seg, r_inter_tanh_s8_2, r_inter_tanh_s4), 1)
 
-        f, f_1, f_2 = self.feats(p) 
+        f, f_1, f_2 = self.feats(p)
         p = self.psp(f)
         inter_s8_3 = self.final_28(p)
-        r_inter_s8_3 = F.interpolate(inter_s8_3, scale_factor=8, mode='bilinear', align_corners=False, recompute_scale_factor=True)
+        r_inter_s8_3 = F.interpolate(
+            inter_s8_3,
+            scale_factor=8,
+            mode='bilinear',
+            align_corners=False,
+            recompute_scale_factor=True,
+        )
 
         p = self.up_1(p, f_2)
         inter_s4_2 = self.final_56(p)
-        r_inter_s4_2 = F.interpolate(inter_s4_2, scale_factor=4, mode='bilinear', align_corners=False, recompute_scale_factor=True)
+        r_inter_s4_2 = F.interpolate(
+            inter_s4_2,
+            scale_factor=4,
+            mode='bilinear',
+            align_corners=False,
+            recompute_scale_factor=True,
+        )
         p = self.up_2(p, f_1)
         p = self.up_3(p, x)
-
 
         """
         Final output
